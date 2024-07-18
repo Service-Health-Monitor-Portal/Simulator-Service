3065ee6062bd41b1c04425cb65f88a83d8a0011c
Mina Magdy <mina.magdy.takawey@gmail.com>
2024-07-18 09:53:47 +0300

    Change origins to '*' and log file's path to "home/simulator.log"

--- src/main/java/com/service_health_monitor_portal/simulator/ServiceController.java
+++ src/main/java/com/service_health_monitor_portal/simulator/ServiceController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController()
 @RequestMapping("/api/v1/services")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class ServiceController {
     @Autowired
     private Log log;
