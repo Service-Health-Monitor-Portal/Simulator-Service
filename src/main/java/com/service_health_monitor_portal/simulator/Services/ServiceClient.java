a9c636eb00a1e4d6483aa26166f981d6d3723628
Mina Magdy <mina.magdy.takawey@gmail.com>
2024-09-17 21:32:46 +0300

    feat(simulator): enhance service fetching error handling and improve logging configuration
    - Added `RestClientException` handling in `ServiceClient` to handle errors during service fetch requests
    - Implemented time-based rolling policy to create hourly log files and compress old logs
    - Set max history to 3 and total size cap to 2GB for log retention
    - Changed log appender from `FILE` to `ROLLING` in `logback.xml` root logger

--- src/main/java/com/service_health_monitor_portal/simulator/Services/ServiceClient.java
+++ src/main/java/com/service_health_monitor_portal/simulator/Services/ServiceClient.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.service_health_monitor_portal.simulator.entity.Service;
@@ -23,9 +24,14 @@ public class ServiceClient {
 
     public List<Service> fetchAllServices() {
         String url = logAnalyzerUrl + "/api/services/all";
-        Service[] services = restTemplate.getForObject(url, Service[].class);
-        System.out.println("Services fetchedddddddddddddddddddddddddd");
-        System.out.println(Arrays.asList(services));
-        return Arrays.asList(services);
+        try {
+            Service[] services = restTemplate.getForObject(url, Service[].class);
+            System.out.println("Services fetched");
+            System.out.println(Arrays.asList(services));
+            return Arrays.asList(services);
+        } catch (RestClientException e) {
+            System.out.println("Error fetching services");
+            return Arrays.asList();
+        }
     }
 }
